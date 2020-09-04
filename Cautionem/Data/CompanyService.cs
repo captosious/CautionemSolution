cc4fd5ddcaf1e98831bdfbe986e323bd06670252
codeforgery <roc.rio@icloud.com>
2020-09-04 12:06:21 +0200

    Up and Go

---
+++ Cautionem/Data/CompanyService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cautionem.Models;
+
+namespace Cautionem.Data
+{
+    public class CompanyService
+    {
+        private MyAppSettings myAppSettings;
+        private CautionemContext cautionemContext;
+
+        public CompanyService(MyAppSettings appSettings, CautionemContext cautionemContext)
+        {
+            this.myAppSettings = appSettings;
+            this.cautionemContext = cautionemContext;
+        }
+
+        public async Task<Company> Get(int companyId)
+        {
+            return (Company)cautionemContext.Company.FirstOrDefault(c=> c.CompanyId == companyId);
+        }
+    }
+}
